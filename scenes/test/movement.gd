405cd20cfc14a15beb000ae0fb03d4276c601cd6
Pedro-CAB <up202006086@up.pt>
2024-03-05 17:13:29 +0000

    Game Camera With Some Bugs

--- scenes/test/movement.gd
+++ scenes/test/movement.gd
@@ -7,13 +7,23 @@ var student_scene = preload("res://scenes/elements/student.tscn")
 func _ready():
 	# How Many Students Should Spawn?
 	$"Level Entrance".student_amount = 20
+	
+func _process(_delta):
+	var direction = Input.get_vector("Move Camera Left","Move Camera Right","Move Camera Up","Move Camera Down")
+	if $Camera2D.position.x + direction.x * 10 > 581 && $Camera2D.position.x + direction.x * 10 < 1725:
+		$Camera2D.position.x += direction.x * 10
+		$"Action Assigner".position.x += direction.x * 10
+	if $Camera2D.position.y + direction.y * 10 > 324 && $Camera2D.position.y + direction.y * 10 < 824:
+		$Camera2D.position.y += direction.y * 10
+		$"Action Assigner".position.y += direction.y * 10
 
 func _on_level_entrance_spawn_student():
 	var student = student_scene.instantiate()
 	student.connect("is_clicked",_on_student_is_clicked)
 	student.connect("is_hovered",_on_student_is_hovered)
 	student.connect("is_not_hovered",_on_student_is_not_hovered)
-	student.scale = Vector2(1.5,1.5)
+	student.position = $"Level Entrance".global_position
+	student.scale = Vector2(3,3)
 	$"Spawned Students".add_child(student,true)
 
 
